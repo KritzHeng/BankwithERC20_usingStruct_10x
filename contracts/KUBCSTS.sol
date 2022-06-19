3be10b3340d4bd83f1efdbff8d11a3e240eb7230
KritzHeng <mr.kriskanin@gmail.com>
2022-06-19 21:07:08 +0700

    add test-contract

---
+++ contracts/KUBCSTS.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol"; 
+
+contract KUBCSTS is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private supply;
+
+    uint256 public maxSupply = 20;
+    string public uriPrefix = "";
+    string public uriSuffix = ".json";
+
+    bool public paused = true;
+
+    constructor() ERC721("KUBCSTS Genesis Pass", "KUBCSTS") {}
+
+    function seturiPrefix(string memory _uriPrefix) public onlyOwner{
+        uriPrefix = _uriPrefix;
+    }
+
+    function seturiSuffix(string memory _uriSuffix) public onlyOwner{
+        uriSuffix = _uriSuffix;
+    }
+
+    function _mintloop(address[] calldata _whitelist) public onlyOwner {
+        require(!paused, "The contract is paused!");
+        require(supply.current() + _whitelist.length <= maxSupply, "Max supply exceeded!");
+
+        for (uint i = 0; i < _whitelist.length; i++) {
+            supply.increment();
+            uint256 newItemId = supply.current();
+            _safeMint(_whitelist[i], newItemId);
+            _setTokenURI(newItemId, string(abi.encodePacked(uriPrefix, Strings.toString(newItemId), uriSuffix) ) );
+        }
+    }
+
+    function setPaused(bool _state) public onlyOwner {
+        paused = _state;
+    }
+
+}
